34ea8cf4d2f18de2b712f775fff0e6f68d364e33
kirankr24 <89823556+kirankr24@users.noreply.github.com>
2021-09-02 09:06:35 +0530

    done

---
+++ madlibs_1.py
@@ -0,0 +1,8 @@
+adj = input("Enter and adjective: ")
+verb1 = input("Enter verb 1: ")
+verb2 = input("Enter verb 2: ")
+famous_person = input("Enter the name of famous person: ")
+
+x = f"Computer programming is so {adj}! It makes me so excited all the time because \n i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(x)
