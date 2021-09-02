8b8ace78179e844cbc8cf04e2be204fc2f77d699
kirankr24 <89823556+kirankr24@users.noreply.github.com>
2021-09-02 09:40:16 +0530

    guess the number

---
+++ guessthenumber_2.py
@@ -0,0 +1,20 @@
+import random
+
+x = int(input("You left side limit is 1 and Enter right side limit: "))
+
+
+
+def guess(x):
+    ran_num = random.randint(1,x)
+    guess = 0
+    while guess != ran_num:
+        guess = int(input(f'Guess number between 1 and {x}: '))
+
+        if guess > ran_num:
+            print("your guess is high")
+        elif guess < ran_num:
+            print("your huess is low")
+    print(f'yay congrats. You have guessed the number {ran_num} correctly!!')
+    quit()
+
+guess(x)
