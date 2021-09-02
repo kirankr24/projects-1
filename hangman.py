e67522629df1aeec77c7cf78c9880914f7eed6ff
kirankr24 <89823556+kirankr24@users.noreply.github.com>
2021-09-02 16:00:56 +0530

    hangman

--- hangman.py
+++ hangman.py
@@ -1,4 +1,12 @@
 import random
 from words import words
+from hangman_visual import lives_visual_dict
 
-print(words)
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    
+    return word.upper()
+
+print(get_valid_word(words))
